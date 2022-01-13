d9327a3e1724fe4a6d498a2eee59277ed0162ff7
pvvvv <qkrdnjsdn77@naver.com>
2022-01-14 08:48:43 +0900

    <feature> : <schedule delete, update, error Page>
    - 스케줄 삭제, 수정 구현
    - 에러페이지 추가

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var userController = require('../routes/user/user-controller');
 var scheduleController = require('../routes/scheduler/scheduler-controller');
 
-/* GET Login page. */
+/* 로그인 페이지로 이동 */
 router.get('/', userController.loginPage);
-/* GET Join page. */
+/* 회원가입 페이지로 이동 */
 router.get('/join', userController.joinPage);
 
 
--- routes/scheduler/index.js
+++ routes/scheduler/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var scheduleController = require('./scheduler-controller');
 
+
+// schedule CRUD
 /* GET schedule page. */
 router.get('/', scheduleController.schedulerPage);
 /* POST schedule page */
@@ -11,8 +13,13 @@ router.put('/', scheduleController.scheduleModify);
 /* delete schedule page */
 router.delete('/', scheduleController.scheduleDelete);
 
-/* GET schedule InsertPage */
+// 페이지 이동
+/* 인서트 페이지로 이동하기 */
 router.get('/insertpage', scheduleController.scheduleInsertPage);
+/* 수정 페이지로 이동하기. */
+router.get('/modify', scheduleController.scheduleModifyPage);
+
+// Ajax
 /* 예약시 시간이 겹치지않도록 데이터를 조회하는 Ajax */
 router.post('/findDate', scheduleController.findDate);
 /* 종료날짜 선택시 시간이 겹치지않도록 데이터를 조회하는 Ajax */
